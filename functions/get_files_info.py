a01e09bdc509ba51c8678714489b1bbc437b5f1c
acesuu <ahnafakib@iut-dhaka.edu>
2025-09-25 14:51:09 +0600

    initialized the project

---
+++ functions/get_files_info.py
@@ -0,0 +1,24 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    
+    abs_working_dir= os.path.abspath(working_directory)
+    abs_directory=""
+    if directory is None:
+        abs_directory = os.path.abspath(working_directory)
+    else:
+        abs_directory = os.path.abspath(os.path.join(working_directory, directory))
+    # print(f"Working dir: {abs_working_dir}, Target dir: {abs_directory} test:{abs_directory.startswith(abs_working_dir)}")
+    
+    if not abs_directory.startswith(abs_working_dir):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+
+    final_res = ""
+    contents = os.listdir(abs_directory)
+    for content in contents:
+        content_path = os.path.join(abs_directory, content)
+        is_dir = os.path.isdir(content_path)
+        size= os.path.getsize(content_path)
+        final_res += f"- {content}: file_size={size} bytes, is_dir={is_dir}\n"
+    return final_res
+        
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+from functions.get_files_info import get_files_info
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    
+
+    if len(sys.argv) < 2:
+        print("I need a prompt as an argument.")
+        sys.exit(1)
+    verboseFlag = False
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verboseFlag = True
+    prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+    print(response.text)
+    if response is None or response.usage_metadata is None:
+        print("No usage metadata available.")
+        return
+    if verboseFlag:
+        print(f"User prompt: {prompt}")    
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+# print(get_files_info("calculator"))
+# main()
